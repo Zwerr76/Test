1d189ec461b19da4c458a7608c6cbdd03d8650d8
Diman <007diman@gmail.com>
2020-05-04 15:05:58 +0300

    Мой первый комит

---
+++ Script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+'Scriptil';
+
+
+//let otvet = prompt("Я вас сюда звал?", "Да");
+
+//console.log(typeof(otvet));
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr);
